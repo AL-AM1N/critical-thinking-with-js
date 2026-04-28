63aad6cd8a0d0f8558c8bec1d5cf7814c6b82035
MD. AL-AMIN <amin57.cse@gmail.com>
2026-04-28 23:28:47 +0600

    Implement stack operations using array

---
+++ module-3/M03V03-stack.js
@@ -0,0 +1,63 @@
+//? Implement stack using array
+
+/**
+ * !Note:
+ * 1. It follows LIFO (Last In First Out)
+ * 2. there are three operations: push(), pop() , peek()
+ * 3. peek() means it will show the top element
+ */
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(value) {
+    this.items.push(value);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  print(){
+    console.log(this.items);
+  }
+}
+
+
+const stack = new Stack();
+
+console.log(stack.pop());
+console.log(stack.isEmpty());
+
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+stack.push(40);
+
+console.log(stack.peek());
+stack.print();
+
+stack.pop();
+console.log(stack.pop());
+
+
+stack.print();
+console.log(stack.peek());
