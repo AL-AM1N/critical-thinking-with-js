ad74c01520ba10ebd1e04c800483ecfad59822fa
MD. AL-AMIN <amin57.cse@gmail.com>
2026-04-28 16:57:32 +0600

    Solve problem (M02V08) using aggregating-and-grouping

---
+++ M02V08-aggregating-and-grouping-data.js
@@ -0,0 +1,51 @@
+//* Grouping and Aggregating Data
+
+// Scenario: You have a flat array of sales data, and you need to group the sales by category,
+// calculating the total revenue and the number of items sold for each.
+
+const sales = [
+  { category: "Electronics", item: "Laptop", price: 1200, quantity: 1 },
+  { category: "Books", item: "JS Basics", price: 30, quantity: 2 },
+  { category: "Electronics", item: "Mouse", price: 25, quantity: 2 },
+  { category: "Home", item: "Chair", price: 150, quantity: 1 },
+  { category: "Books", item: "React Deep Dive", price: 50, quantity: 1 },
+  { category: "Electronics", item: "Keyboard", price: 80, quantity: 1 },
+];
+
+//? Output
+// {
+//   Electronics: {
+//     totalRevenue: 1330,
+//     itemCount: 4,
+//   },
+//   Books: {
+//     totalRevenue: 110,
+//     itemCount: 3,
+//   },
+//   Home: {
+//     totalRevenue: 150,
+//     itemCount: 1,
+//   },
+// };
+
+//? Solution:
+
+const result = sales.reduce((acc, sale) => {
+    
+    const {category, price, quantity} = sale;
+
+  if (!acc[sale.category]) {
+    acc[sale.category] = {
+      totalRevenue: 0,
+      itemCount: 0,
+    };
+  }
+
+  acc[sale.category].totalRevenue = acc[sale.category].totalRevenue + (sale.price * sale.quantity);
+
+  acc[sale.category].itemCount = acc[sale.category].itemCount + sale.quantity;
+
+  return acc;
+}, {});
+
+console.log(result);
