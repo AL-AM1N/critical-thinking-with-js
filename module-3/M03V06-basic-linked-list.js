b9777c3908ed9b6b8a76f46b56b63bb57a6b64c4
MD. AL-AMIN <amin57.cse@gmail.com>
2026-04-29 12:43:09 +0600

    Implement linked list append operation

--- module-3/M03V06-basic-linked-list.js
+++ module-3/M03V06-basic-linked-list.js
@@ -1,11 +1,10 @@
 //? Linked List Implementation - Basic concept of Node
 
 class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
-
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 const head = new Node(10);
@@ -15,11 +14,7 @@ console.log(head);
 
 let temp = head;
 
-while(temp !== null ){
-    console.log(temp.value, " ");
-    temp = temp.next;
+while (temp !== null) {
+  console.log(temp.value, " ");
+  temp = temp.next;
 }
-
-
-
-
